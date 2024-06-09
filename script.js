ba6321861ebb64e1725e460a03e5b34244af608d
Anish <56240492+AG-Coding@users.noreply.github.com>
2024-06-09 01:53:41 -0700

    idk mehh

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+    });
+
+    const $cont = document.querySelector('.cont');
+    const $elsArr = [].slice.call(document.querySelectorAll('.el'));
+    const $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+    setTimeout(() => {
+        $cont.classList.remove('s--inactive');
+    }, 200);
+
+    $elsArr.forEach($el => {
+        $el.addEventListener('click', function () {
+            if (this.classList.contains('s--active')) return;
+            $cont.classList.add('s--el-active');
+            this.classList.add('s--active');
+        });
+    });
+
+    $closeBtnsArr.forEach($btn => {
+        $btn.addEventListener('click', function (e) {
+            e.stopPropagation();
+            $cont.classList.remove('s--el-active');
+            document.querySelector('.el.s--active').classList.remove('s--active');
+        });
+    });
+});
