739a7530c68d429b877da4ab5337a67444ab634d
Anish <56240492+AG-Coding@users.noreply.github.com>
2024-06-09 11:41:50 -0700

    works

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'camera.html'));
 });
 
 app.get('/gauthenticate', async (req, res) => {
